4df5bc1e188b6c5fff8dbc1c4f59a16c389d1e4e
avoe <inuser027@gmail.com>
2024-06-06 23:26:25 +0400

    Update

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.window_height=640
-    page.window_width=360
     page.title = "avoe.dev ECO project"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
-    def isbannertrue_fun(isbannertrue):
-        isbannertrue=True
-        return isbannertrue
     
 
     page.add(
@@ -21,25 +14,20 @@ def main(page: ft.Page):
                             title=ft.Text("Project is under development! There may be many errors and bugs, please report a bug if you found any. Thank you!")
                         ),
                         ft.Row(
-                            [ft.ElevatedButton("Learn more",icon=ft.icons.INSERT_LINK_OUTLINED),ft.ElevatedButton("Report a bug",icon=ft.icons.BUG_REPORT,on_click=isbannertrue_fun), ft.ElevatedButton("Support project",icon=ft.icons.ATTACH_MONEY)],
+                            [ft.ElevatedButton("Learn more",icon=ft.icons.INSERT_LINK_OUTLINED),ft.ElevatedButton("Report a bug",icon=ft.icons.BUG_REPORT), ft.ElevatedButton("Support project",icon=ft.icons.ATTACH_MONEY)],
                             alignment=ft.MainAxisAlignment.END,
                         ),
                     ]
                 ),
                 padding=10,
+                bgcolor=ft.colors.PURPLE,
                 border_radius=10,
                 alignment=ft.alignment.top_center
             )
         ),
-        ft.Banner(
-            ft.Row([
-                ft.Text("Development mode"),
-                ft.ElevatedButton("Learn more"),
-                ft.ElevatedButton("Dismiss"),
-            ]),
-            bgcolor="gray",
-            open=True
-        ),
+        ft.Row(
+            
+        )
     )
 
 
